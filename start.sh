debacbcc4391a6b351b84e1dd6745abdb86ab811
Garth Cumming <garthman@gmail.com>
2014-10-01 10:24:27 -0700

    Added config for the talk proxy, set hostnames for vancouver.hackspace.ca

--- start.sh
+++ start.sh
@@ -10,5 +10,6 @@ echo "Starting"
 docker run -p 80:80 -p 443:443 -d --name vhs-nginx \
     --volumes-from vhs-website \
     --link vhs-website:vhs-website \
+    --link app:app \
     -v $DIR/logs:/var/log/nginx \
     -t hackspace/vhs-nginx $1
