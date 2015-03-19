f965254ba544bfd42a18541968db6f8815bdde81
James Sapara <james.sapara@gmail.com>
2015-03-18 20:57:10 -0700

    faster builds by piping tar

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 docker build -t hackspace/vhs-nginx $DIR
+tar -c nginx.conf ./conf.d/ ./certs/ ./bin/|docker build -t hackspace/vhs-nginx $DIR
