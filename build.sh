71bf6e28edde95d41dc8fae1972cfb84928fcc80
root <root@mail.hackspace.ca>
2015-05-15 23:27:58 -0700

    Merge of build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 docker build -t hackspace/vhs-nginx $DIR
-<<<<<<< HEAD
-=======
 tar -c nginx.conf ./conf.d/ ./certs/ ./bin/|docker build -t hackspace/vhs-nginx $DIR
->>>>>>> d581767496ec9396c7bf628d7af88932f782b260
